b28669049b14be114582b4631c21f87b3b14c2d2
Mauricio Bedun <mauriciobedun@hotmail.com>
2023-09-12 20:50:26 -0300

    ap04(repeticao): for

---
+++ Pares1a1000.java
@@ -0,0 +1,9 @@
+//mostrar os pares de 1 a 1000, usando for
+public class Pares1a1000 {
+    public static void main(String[] args) {
+        System.out.println("pares de 1 a 1000");
+        for (int cont=2; cont <= 1000; cont = cont + 2) {
+            System.out.print(cont + " ");
+        }
+    }
+}
